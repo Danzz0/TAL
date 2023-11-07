8ea9cea906f4d76201cfffdf71e00c978ddb087f
Danilo Lima Gomes <20221144010024@ifrn.local>
2023-11-07 10:25:23 -0300

    Criando menu

--- src/views/BibliotecaUI.ts
+++ src/views/BibliotecaUI.ts
@@ -1,22 +1,24 @@
-import * as readlineSync from 'readline-sync'
+import * as rl from 'readline-sync'
 import {Livro, Produto, Estoque} from '../models/exportador'
+
 export class BibliotecaUI{
     
 
 
-    public registrarLivro(): void{
+    public registrarLivro(): void{ // está aceitando valores inválidos (controller tem que ajeitar)
         let titulo:string;
         let autor:string;
         let editora:string; 
         let info:string;
         let preco:number;
         console.log("===========Biblioteca===========");
-        console.log("     Deseja criar um livro?     ");
-        titulo = readlineSync.question('Qual é o seu título? ');
-        editora = readlineSync.question('Qual é a sua editora? ');
-        info = readlineSync.question('Qual é o a sua sinópse? ');
-        autor = readlineSync.question('Qual é o seu autor? ');
-        preco = readlineSync.question('Qual é o seu preço? ');
+        console.log("     Deseja criar um livro?     \n");
+        //const prompt = iconv.decode(rl.question(iconv.encode("Digite sua mensagem:", "utf8")), 'utf8');
+        titulo =  rl.question('Qual o titulo? ');
+        editora = rl.question('Qual a sua editora? ');
+        info = rl.question('Qual a sua sinopse? ');
+        autor = rl.question('Qual o seu autor? ');
+        preco = rl.question('Qual o seu preco? R$');
         console.log("processando dados... \n")
         
 
@@ -27,6 +29,24 @@ export class BibliotecaUI{
 
         console.log(estoq.produtos);
     }
+
+    public menu(){
+
+        let options = ['Registrar um livro', 'Registrar conta de cliente', 'Registrar cartão de crédito']
+
+        console.log("===========Biblioteca===========");
+        console.log("              MENU            \n");
+
+        let index =  rl.keyInSelect(options, 'Which animal?');
+        /*
+            animals = ['Lion', 'Elephant', 'Crocodile', 'Giraffe', 'Hippo'],
+            index = readlineSync.keyInSelect(animals, 'Which animal?');
+            console.log('Ok, ' + animals[index] + ' goes to your room.');
+        */
+        console.log("Criar livro: ")
+    }
+
+    
 }
 
 /*"Game of Thrones", "J. R. R. Martin", "Viena", new Date(), "OJASNISAI", 25*/
